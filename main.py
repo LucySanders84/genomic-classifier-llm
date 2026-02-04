0eba41c3caf5bf82151ad3b7ecb1b4c3604a57f6
L. Sanders <lucyhsanders@gmail.com>
2026-02-04 14:27:21 -0700

    added comments with process steps

---
+++ main.py
@@ -0,0 +1,24 @@
+# Prepare resources
+    #create requirements.txt detailing libraries that need to be installed
+    #import required libraries
+
+#Configure fine - tuning
+
+#Prepare the tokenizer
+
+#Prepare data
+
+#Load and Configure the Model for Sequence Classification
+
+#Initialize the Trainer
+
+#Start the training
+
+#Evaluate Model Performance
+
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
