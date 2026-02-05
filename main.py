b0ee8a59d56da1ccf7823558dc8894033a4e3039
L. Sanders <lsanders49@student.umgc.edu>
2026-02-05 10:24:38 -0700

    added additional comments

--- main.py
+++ main.py
@@ -2,14 +2,16 @@
     #create requirements.txt detailing libraries that need to be installed
     #import required libraries
 
-#Configure fine - tuning
-
+#Configure fine-tuning
+    #PEFT
+    #QLoRAs
 #Prepare the tokenizer
 
 #Prepare data
+    #create pipeline to add fasta sequences to train.csv, test.csv files
 
 #Load and Configure the Model for Sequence Classification
-
+    #DNABERT-2
 #Initialize the Trainer
 
 #Start the training
