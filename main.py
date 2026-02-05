bf2a453a3ea02196544139ab966293c3eb2bc941
L. Sanders <lsanders49@student.umgc.edu>
2026-02-05 11:10:36 -0700

    added additional comments under prepare data

--- main.py
+++ main.py
@@ -9,6 +9,12 @@
 
 #Prepare data
     #source coding and non-coding sequences
+        #annotated genes are coding sequences
+        #non-coding seq idea
+            #remove gene sequences from genome
+            #check for remaining sequences starting with ATG and ending with stop codon and remove
+            #remainder of genome is non-coding?
+            #break into sequences 500bp long
     #create pipeline to add fasta sequences to train.csv, test.csv files
 
 #Load and Configure the Model for Sequence Classification
