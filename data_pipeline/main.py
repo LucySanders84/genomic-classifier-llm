b3f3fb55d0e086d9bac6ada73a096e602397f800
L. Sanders <lsanders49@student.umgc.edu>
2026-02-07 21:45:41 -0700

    added comments for data pipeline code

--- data_pipeline/main.py
+++ data_pipeline/main.py
@@ -1,19 +1,4 @@
 def main():
-    #data pipeline tasks
-    #coding sequences
-        #add sequence to csv file
-            #get sequence file
-            #parse sequence into single str
-            #open coding_sequences csv file
-            #add sequence and label to csv file
-    #non-coding sequences
-        #concat full candida genome
-        #for gene fasta files
-            #parse header for location
-            #remove location from genome file
-        #split remaining genome into 500bp segments
-        #test whether segment is coding or not
-        #append segments that don't code to csv file
     #create pipeline script to generate csv files
     pass
 
