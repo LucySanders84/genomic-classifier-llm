b359645b7ae050aadc1f319ab8a9bffef51ad00b
L. Sanders <lsanders49@student.umgc.edu>
2026-02-20 10:24:06 -0700

    initial commit

--- data_pipeline/pipeline.py
+++ data_pipeline/pipeline.py
@@ -2,6 +2,8 @@
 from typing_extensions import Pattern
 
 from data_pipeline import exons_introns_pipeline
+from data_pipeline.constants import DATA_PATH
+from data_pipeline.pipeline_lib import datasets
 from data_pipeline.pipeline_lib.inputs import inputs as inputs_lib
 
 """
@@ -21,21 +23,32 @@ def run(data_source_parameters: dict[str, dict[str, str | Pattern ]], input_para
     #       -> OUTPUT list[tuple[sequence: str, label:str]]
 
     inputs = inputs_lib.build(data_source_parameters, input_parameters)
-    print(f'{len(inputs)} inputs created from the following files:')
+    for label in inputs:
+        print(f'Group {label}: {len(inputs[label])} inputs passed validation steps')
     for key in data_source_parameters["fsf"].keys():
         print("\t", data_source_parameters["fsf"][key])
 
     # INPUT list[tuple(sequence: str, label:str)
     #   -> validate fragment output
     #       -> OUTPUT list[str (values separated by tabs)]
+    validated_data = inputs_lib.validate(inputs, input_parameters["max_n_percentage"], input_parameters["length"])
 
-    validated_inputs = inputs_lib.validate(inputs, input_parameters["max_n_percentage"], input_parameters["length"])
-    print(f'{len(validated_inputs)} inputs created after validation steps')
     # INPUT list[str (values separated by tabs)]
-    #   -> balance 1 and 0 inputs across train, test, val sets
-    #       -> WRITE TO FILE
+    # balance 1 and 0 inputs across train, test, val sets
+    data_splits = inputs_lib.balance(validated_data)
+    # WRITE TO FILE
+    datasets.write_from_data_splits(data_splits, DATA_PATH, 'tsv', '\t', '\n')
 
     pass
+
+
+# Balance and Split Genomics Data
+import random
+import os
+
+
+# This group separates out how many coding and non-coding fragments are actually present
+
 
 if __name__ == '__main__':
     #runs exons and introns pipeline as default
---
+++ data_pipeline/pipeline_lib/datasets.py
@@ -0,0 +1,24 @@
+import os
+
+def format_inputs_for_dataset(data: list[tuple[str, str]], line_delimiter, col_delimiter, output_format='list'):
+    try:
+        if output_format == 'list':
+            return col_delimiter.join([line_delimiter.join(line) for line in data])
+        else:
+            raise ValueError('Only output format of "list" is currently supported.')
+    except ValueError as e:
+        print(e)
+
+
+def write_from_data_splits(data_splits: dict[str, list[tuple[str,str]]], output_dir, file_extension: str, line_delimiter:str, col_delimiter: str):
+    # This writes to file system
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+
+    for filename, data in data_splits.items():
+
+        path = os.path.join(output_dir, filename + '.' + file_extension)
+        formatted_data = format_inputs_for_dataset(data, line_delimiter, col_delimiter)
+        with open(path, 'w') as f:
+            f.writelines(formatted_data)
+        print(f"Successfully created {len(data)} with training data sequences to {path}")
